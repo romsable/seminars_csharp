0fcb4a33f263fc8a8fa556969fc6c3dedc79cd39
sable <rom.sable@gmail.com>
2024-02-21 23:47:08 +0400

    решили первую задачку

--- seminar01/task01/Program.cs
+++ seminar01/task01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 == number2 * number2)
+{
+    Console.WriteLine("да");
+}  
+else
+{
+    Console.WriteLine("нет");
+}
