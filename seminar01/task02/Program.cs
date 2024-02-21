2484c6dd5588e7636d756e9a673a94701bfd5a1d
sable <rom.sable@gmail.com>
2024-02-22 01:09:25 +0400

    решили вторую задачку

--- seminar01/task02/Program.cs
+++ seminar01/task02/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = - number;
+
+while (index <= number)
+{
+    Console.Write($"{index} ");
+     index++;
+}  
