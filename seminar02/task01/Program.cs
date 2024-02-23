aafb529748167850ce01670985de31c956463b90
sable <rom.sable@gmail.com>
2024-02-23 23:55:29 +0400

    решил вторую задачку

--- seminar02/task01/Program.cs
+++ seminar02/task01/Program.cs
@@ -1,8 +1,24 @@
 ﻿// Напишите программу, которая принимает на вход 
 // трёхзначное число и удаляет вторую цифру этого числа.
 
-int number = new Random().Next(100,1000);
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+// int s = number/100*10 + number%10;
+// Console.WriteLine(s);
+// Console.WriteLine(number/100 + "" + number%10);
+
+
+// Напишите программу, которая принимает на вход 
+// трёхзначное число и возводит вторую цифру этого 
+// числа в степень, равную третьей цифре.
+
+int number = new Random().Next(10,1000);
 Console.WriteLine(number);
-int s = number/100*10 + number%10;
-Console.WriteLine(s);
-Console.WriteLine(number/100 + "" + number%10);
+if(number <= 999 && number >= 100)
+{
+    Console.WriteLine(Math.Pow(number/10%10, number%10));
+}
+else 
+{
+    Console.WriteLine("Число не трехзначное");
+}
