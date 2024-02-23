89cf1ffcc236d1482353c6b1e47a6eba66aea5b0
sable <rom.sable@gmail.com>
2024-02-23 23:09:58 +0400

    решили первую задачу второго семинара

--- seminar02/task01/Program.cs
+++ seminar02/task01/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+int s = number/100*10 + number%10;
+Console.WriteLine(s);
+Console.WriteLine(number/100 + "" + number%10);
