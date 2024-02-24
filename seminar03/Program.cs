67f8db99834b7411b6b589e3403f24d361540205
sable <rom.sable@gmail.com>
2024-02-24 23:25:23 +0400

    решили 4 задачу из дз

--- seminar03/Program.cs
+++ seminar03/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// решение задачи 4 из дз
+
+Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n;
+int count = 0;
+while (m > 0)
+{
+    count++;
+    m /= 10;
+}
+while (count > 0)
+{
+    Console.Write($"{n / Convert.ToInt32(Math.Pow(10, count-1))%10}, ");
+    count--;
+} 
