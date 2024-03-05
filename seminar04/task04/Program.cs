0069634ffe3fd865462ed6be959beabda0feef39
sable <rom.sable@gmail.com>
2024-03-05 23:17:57 +0400

    add dztask1

--- seminar04/task04/Program.cs
+++ seminar04/task04/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая бесконечно запрашивает 
+// целые числа с консоли. Программа завершается при вводе 
+// символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+using System.Linq.Expressions;
+
+bool sumnumber (int n)
+{
+    string m = Convert.ToString(n);
+    Console.WriteLine(m);
+    int sum = 0;
+    foreach (var item in m)
+    {
+       if (char.IsDigit(item))
+       {
+        int digit = item - '0';
+        sum += digit;
+       }
+    }
+    Console.WriteLine(sum);
+if (sum % 2 == 0) return true;
+return false;
+}
+
+while (true)
+{
+    Console.Write("Введите целое число (или символ q для выхода): ");
+     string input = Console.ReadLine();
+     int number;
+    if (input == "q")
+    {
+        break;
+    }
+    int.TryParse(input, out number);
+   if ( sumnumber(number))
+    {
+        break;
+    }
+    else
+    {
+        continue;
+    }
+}
